4a2a6c15afd275a1eceec1d48431074843138c4b
Bikash Shaw <1985.bikash.shaw@gmail.com>
2022-05-21 13:24:16 +0530

    codebackup for store dashboard

---
+++ CodeBackUp/store-dashboard/src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Login from "./Login";
+import Register from "./Register";
+import NoMatchPage from "./NoMatchPage";
+import Dashboard from "./Dashboard";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import { UserContext } from "./UserContext";
+import Store from "./Store";
+
+function App() {
+  let [user, setUser] = useState({
+    isLoggedIn: false,
+    currentUserId: null,
+    currentUserName: null,
+  });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <HashRouter>
+        <NavBar />
+        <div className="container-fluid">
+          <Routes>
+            <Route path="/" exact="true" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="*" element={<NoMatchPage />} />
+          </Routes>
+        </div>
+      </HashRouter>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
